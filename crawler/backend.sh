4a2403d68dd00880276bc8725f5284b61298943e
Arun Kumar M S <arunkmsn@gmail.com>
2019-03-08 08:26:53 +0000

    changes to make it work gcloud

--- crawler/backend.sh
+++ crawler/backend.sh
@@ -1,2 +1,2 @@
-nohup celery -A spider worker -l info &
-nohup gunicorn app:app &
+nohup celery -A spider worker -l info > $HOME/celery.log &
+nohup sudo ~/crawler/crawler/env/bin/flask run -h '0.0.0.0' -p 80 > $HOME/gunicorn.log &
